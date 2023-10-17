7d6cdcdfa8075a3b91d7716e3b76936627397ba8
ase-tech <hareesh@asetechnologies.in>
2023-10-17 12:11:55 +0530

    Dr. Shalini

---
+++ footer.php
@@ -0,0 +1,118 @@
+<style type="text/css">
+  .floating-wpp-button{
+    height: 50px!important;
+    width: 50px!important;
+  }
+  .floating-wpp-head span{
+    color: #fff!important;
+  }
+  .floating-wpp{
+    z-index: 999!important;
+  }
+</style>
+
+<footer class="ftco-footer ftco-section">
+		<div class="container">
+		     <div id="WAButton"></div>
+			<div class="row mb-5">
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Lets talk about</h2>
+						<p>Dr. Shalini Duvvada, a renowned General, Laparoscopic and Breast Surgeon, practicing in the city of Vishakhapatnam is also dedicated towards service to humanity for which she runs a Trust that provides treatment to cancer patients.</p>
+						<p><a href="about.php" class="btn btn-primary">Learn more</a></p>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+						<h2 class="ftco-heading-2">Links</h2>
+						<ul class="list-unstyled">
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Home</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Services</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Events</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Appointment</h2>
+						<ul class="list-unstyled">
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Evening : 4pm to 6.30pm</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Morning : On Appointment </a></li>
+							<!--<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Thyroid disorders</a></li>-->
+							<!--<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Gastritis and Pain abdomen </a></li>-->
+							<!--<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Laparoscopic hernia surgeries </a></li>-->
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Have a Questions?</h2>
+						<div class="block-23 mb-3">
+							<ul>
+								<li><span class="icon fa fa-map marker"></span><span class="text">#2-100-10/A, Geetam Medical College Road, Revenue ward-6, Yendada, Vishakhapatnam </span></li>
+								<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+91 8297758676</span></a></li>
+								<li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">shalinidkwel@gmail.com</span></a></li>
+							</ul>
+						</div>
+						<ul class="ftco-footer-social list-unstyled mt-2">
+							<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+							<li class="ftco-animate"><a href="https://www.facebook.com/people/DrShalini-Duvvada/100076191106959/?mibextid=ZbWKwL"><span class="fa fa-facebook"></span></a></li>
+							<li class="ftco-animate"><a href="https://www.instagram.com/shalini_duvvada/?igshid=YmMyMTA2M2Y%3D"><span class="fa fa-instagram"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+
+					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Designed by <a href="#" target="_blank">Ase Technologies</a>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		
+
+		<!-- loader -->
+		<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery-migrate-3.0.1.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="js/jquery.waypoints.min.js"></script>
+		<script src="js/jquery.stellar.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/jquery.animateNumber.min.js"></script>
+		<script src="js/scrollax.min.js"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+		<script src="js/google-map.js"></script>
+		
+		<script src="js/main.js"></script>
+		
+		
+		<link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
+<!--Floating WhatsApp javascript-->
+<script type="text/javascript" src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.js"></script>
+
+		<script type="text/javascript">  
+   $(function () {
+                $('#WAButton').floatingWhatsApp({
+               phone: '+918297758676', //WhatsApp Business phone number
+               headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+               popupMessage: 'Hello, how can we help you?', //Popup Message
+               showPopup: true, //Enables popup display
+               buttonImage: '<img src="images/whatsapp.png" />',
+               position: "right" //Position: left | right
+           });
+       });
+</script> 
+	</body>
+	</html>
---
+++ index.php
@@ -0,0 +1,406 @@
+<?php include 'meta_header.php'; ?>
+<style type="text/css">
+	.media-body p{
+		color: green;
+		height:350px;
+		/*font-weight: bold;*/
+	}
+		.media-body{
+		    height:400px;
+		}
+		.services .media-body h3 {
+    font-size: 18px;
+    font-weight: bold!important;
+}
+</style>
+<section id="home-section" class="hero">
+		<div class="home-slider owl-carousel">
+			<div class="slider-item">
+				<div class="overlay"></div>
+				<div class="container-fluid px-md-0">
+					<div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+						<div class="one-third order-md-last img" style="background-image:url(images/24.jpeg);">
+							<div class="overlay"></div>
+							<div class="overlay-1"></div>
+						</div>
+						<div class="one-forth d-flex  align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+							<div class="text slidertext">
+								<span class="subheading">Dr.Shalini duvvada</span>
+								<h1 class="mb-4 mt-3"><b>Specialist <span>in<br> Breast Diseases </span></b> </h1>
+								<p><a href="tel:+8297758676" class="btn btn-primary">Appointment</a> <a href="#" class="btn btn-primary btn-outline-primary">Contact</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="slider-item">
+				<div class="overlay"></div>
+				<div class="container-fluid px-md-0">
+					<div class="row d-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+						<div class="one-third order-md-last img" style="background-image:url(images/26.jpeg);">
+							<div class="overlay"></div>
+							<div class="overlay-1"></div>
+						</div>
+						<div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+							<div class="text">
+								<span class="subheading">Treatments</span>
+								<h1 class="mb-4 mt-3"><b>Expert in</b><br> <b><span>Laparoscopic<br> Surgeries </span></b> </h1>
+								<p><a href="#" class="btn btn-primary">Appointment</a> <a href="#" class="btn btn-primary btn-outline-primary">Contact</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</section>
+<marquee>We are working fully during covid times with all precautions to protect our patients and health workers.
+Please do not ignore your health. Seek consultations in time.</marquee>
+	<section class="ftco-counter img bg-light" id="section-counter">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+					<div class="block-18 d-flex">
+						<div class="icon d-flex justify-content-center align-items-center">
+							<span class="flaticon-suitcase"></span>
+						</div>
+						<div class="text">
+							<strong class="number" data-number="5000">0</strong>
+							<span>No. Of Surgery</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+					<div class="block-18 d-flex">
+						<div class="icon d-flex justify-content-center align-items-center">
+							<span class="flaticon-loyalty"></span>
+						</div>
+						<div class="text">
+							<strong class="number" data-number="10000">0</strong>
+							<span>Happy Patients</span>
+						</div>
+					</div>
+				</div>
+				<!--<div class="col-md-4 justify-content-center counter-wrap ftco-animate">-->
+				<!--	<div class="block-18 d-flex">-->
+				<!--		<div class="icon d-flex justify-content-center align-items-center">-->
+				<!--			<span class="flaticon-coffee"></span>-->
+				<!--		</div>-->
+				<!--		<div class="text">-->
+				<!--			<strong class="number" data-number="10">0</strong>-->
+				<!--			<span>Year Of Experience</span>-->
+				<!--		</div>-->
+				<!--	</div>-->
+				<!--</div>-->
+				<div class="col-md-4 justify-content-center counter-wrap ftco-animate">
+					<div class="block-18 d-flex">
+						<div class="icon d-flex justify-content-center align-items-center">
+							<span class="flaticon-calendar"></span>
+						</div>
+						<div class="text">
+							<strong class="number" data-number="10">0</strong>
+							<span>YEAR OF EXPERIENCE</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+	
+	
+
+	<section class="ftco-section" id="services-section">
+		<div class="container">
+			<div class="row justify-content-center opa-col">
+				<div class="col-md-12 heading-section text-center ftco-animate mb-5">
+					<!-- <span class="subheading">I am grat at</span> -->
+					<h2 class="mb-4">OUR TREATMENTS</h2>
+					<p class="text">Trained cancer surgeon focusing only on breast cancer. 10+ years of experience in Breast conservation and reconstruction.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon d-flex align-items-center justify-content-center"><span class=""><img src='images/breast-cancer.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">BREAST DISEASES</h3>
+							<p>Breast changes are experienced by almost all women. There can be lumps, bumps and discharges that may lead to breast diseases. If you experience any of these you may contact us as your health care provider.  </p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/examination.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">PILES, FISSURE AND FISTULA</h3>
+							<p>These are all anal pathologies. The terminal part of the anus has sensitive nerve endings, which when affected leads to either Piles, Fissures or Fitsula.  </p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/thyroid.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">THYROID DISORDERS</h3>
+							<p>A butterfly shaped gland is found at the base of the neck. Any dysfunction of it is what leads to Thyroid Disorder. Thyroid disorders are a small, harmless goiter that needs no treatment to life-threatening cancer. 
+</p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/gastric.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">GASTRITIS AND PAIN ABDOMEN </h3>
+							<p>Gastritis is a condition that inflames the stomach lining causing belly pain, indigestion, bloating and nausea. It can lead to other problems. Gastritis can come on suddenly or gradually.  </p>
+						</div>
+					</div> 
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/laparoscope.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">LAPRAOSCOPIC HERNIA SURGERIES </h3>
+							<p>Laparoscopic hernia repair uses a laparoscope, a thin, telescope-like instrument that is inserted through a small incision at the umbilicus. This procedure is usually performed under general anesthesia. 
+ </p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"<span class=""><img src='images/varicose-veins.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">VARICOSE VEINS</h3>
+							<p>Varicose veins are twisted, enlarged veins. Any vein that is close to the skin's surface can become varicosed. Varicose veins most commonly affect the veins in the legs.  </p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/diabetes.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">DIABETIC FOOT DISORDERS</h3>
+							<p>Foot problems are common in people with diabetes. They can happen over time when high blood sugar damages the nerves and blood vessels in the feet. They can cause numbness, tingling, pain, or a loss of feeling in your feet.  </p>
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="media block-6 services d-block bg-white rounded-lg shadow ftco-animate">
+						<div class="icon shadow d-flex align-items-center justify-content-center"><span class=""><img src='images/trauma.png' height="50px" width="50px"></span></div>
+						<div class="media-body">
+							<h3 class="heading mb-3">TRAUMA-STITCHES</h3>
+							<p>Most minor cuts and scrapes heal on their own, with little more intervention needed than mild soap and water to keep them clean.</p>
+						</div>
+					</div> 
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-hireme">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-md-8 col-lg-8 d-flex align-items-center">
+					<div class="w-100 py-4">
+						<h2>LAPAROSCOPIC SURGERY</h2>
+						<p>Laparoscopic Surgeries are small incision surgeries performed in the abdomen region or pelvis with the use of a camera.</p>
+						<p class="mb-0"><a href="#" class="btn btn-white py-3 px-4">Contact me</a></p>
+					</div>
+				</div>
+				<div class="col-md-4 col-lg-4 d-flex align-items-end">
+					<video width="600" height="400" controls>
+					  <source src="videos/123.mp4" type="video/mp4">
+					</video>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="ftco-section bg-light" id="blog-section">
+		<div class="container">
+			<div class="row justify-content-center mb-5 pb-5">
+				<div class="col-md-7 heading-section text-center ftco-animate">
+					<!-- <span class="subheading">Videos</span> -->
+					<h2 class="mb-4">Specialities</h2>
+					
+				</div>
+			</div>
+			<div class="row d-flex">
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry justify-content-end">
+						<a href="#" class="block-20" style="background-image: url('images/6.jpeg');">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Fellowship in minimal access surgery from the prestigious world laparoscopy Hospital</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry justify-content-end">
+						<a href="#" class="block-20" style="background-image: url('images/new.jpeg');">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Open Hernia surgery..................</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry">
+						<a href="#" class="block-20" style="background-image: url('images/8.jpeg');">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Well trained by eminent surgeons of India</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row d-flex">
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry justify-content-end">
+						<a href="#" class="block-20" style="background-image: url('images/27.jpeg');">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Trained at World laparoscopy hospital</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry justify-content-end">
+						<a href="#" class="block-20" style="background-image: url('images/28.jpeg');background-size: contain;">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Advanced dressing for skin burns and injuries</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 d-flex ftco-animate">
+					<div class="blog-entry">
+						<a href="#" class="block-20" style="background-image: url('images/29.jpeg');">
+						</a>
+						<div class="text mt-3 float-right d-block">
+							<div class="d-flex align-items-center mb-3 meta">
+								<!--<p class="mb-0">-->
+								<!--	<span class="mr-2">July 03, 2020</span>-->
+								<!--	<a href="#" class="mr-2">Admin</a>-->
+								<!--	<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>-->
+								<!--</p>-->
+							</div>
+							<h3 class="heading"><a href="#">Emergency abdomen surgeries</a></h3>
+							<!--<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<section class="ftco-section testimony-section bg-primary" style="background-color: #fff!important;">
+		<div class="container">
+			<div class="row justify-content-center pb-5" style="padding-bottom: 0rem !important;">
+				<div class="col-md-12 heading-section heading-section-white text-center ftco-animate">
+					<span class="subheading" style="color:deeppink!important;">Testimonies</span>
+					<h2 class="mb-4" style="color:deeppink;">What patient says about?</h2>
+					<!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p> -->
+				</div>
+			</div>
+			<div class="row ftco-animate">
+				<div class="col-md-12">
+					<div class="carousel-testimony owl-carousel">
+						<div class="item">
+							<div class="testimony-wrap py-4">
+								<div class="text">
+									<span class="fa fa-quote-left"></span>
+									<p style="font-weight:bold;!important" class="mb-4 pl-5">Doctor Shalini is very professional at her work and very committed to the treatment. She takes every pain of the patient and gives them moral support for the patient which seriously a patient requires at that time of the illness. Available at any time to the query of the patient. Simply a Doctor who determines herself for the service at 100% towards the recovery of the patient in a short time.</p>
+								
+										<p style="float:right;!imporant;color:green!important;"><b>--S. Puspavathi</b></p>
+									
+									
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="testimony-wrap py-4">
+								<div class="text">
+									<span class="fa fa-quote-left"></span>
+									<p style="font-weight:bold;!important" class="mb-4 pl-5">I’m happy with Doctor explanation of my health issue and she gave me good prescription and diet. She explained everything clearly.Good patient listener, Will suggest this doctor to all. Thank you mam.</p>
+									<p style="float:right;!imporant;color:green!important;"><b>--Yellapu Satish</b> </p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="testimony-wrap py-4">
+								<div class="text">
+									<span class="fa fa-quote-left"></span>
+									<p style="font-weight:bold;!important" class="mb-4 pl-5">She is very friendly. Explain everything clearly. Good patient listener. Will suggest this doctor to all. Tq..</p>
+									<p style="float:right;!imporant;color:green!important;"><b>--Pawan.G</b></p>
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="testimony-wrap py-4">
+								<div class="text">
+									<span class="fa fa-quote-left"></span>
+									<p style="font-weight:bold;!important" class="mb-4 pl-5">I have consulted her for treatment of hand swelling post hospitalization. She patiently listened to whatever we have said to understand the complete medical history and suggested the right medication. After using the medication prescribed by her, swelling has been reduced.</p>
+									<p style="float:right;!imporant;color:green!important;"><b>--Verified patient</b>.</p>
+								</div>
+							</div>
+						</div>
+				
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+	<?php include 'footer.php'; ?>
